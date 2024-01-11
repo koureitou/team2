9b7037d6c699c260117261c9fd6286421594e922
koureitou <kou.reitou@casc.co.jp>
2024-01-11 18:04:12 +0900

    update

---
+++ team2/src/test/java/com/csc/api/Team2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csc.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
