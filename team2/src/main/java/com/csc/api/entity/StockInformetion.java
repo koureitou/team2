8697e14e4c6528f980ba5ac94e8012399612c3bb
kwfeng1211 <kwfeng1211@gmail.com>
2024-01-17 13:43:13 +0900

    Update Files
    Update Files 1st

---
+++ team2/src/main/java/com/csc/api/entity/StockInformetion.java
@@ -0,0 +1,35 @@
+package com.csc.api.entity;
+/*
+ * author:フォン
+ * data:2024 01/10
+ */
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class StockInformetion {
+
+	private String stockId;//在庫商品ID
+	
+	private String stockname;//在庫商品名称
+
+	private String unitid;//単位ID
+
+	private String stocknum;//在庫数量
+
+	private String remarks; //備考
+
+	private String delflg;//削除フラグ
+
+	private String createdata;//作成日時
+
+	private Integer createuser;//作成ID
+
+	private String updatedate;//更新日時
+
+	private Date updateuser;//更新者ID
+
+	private Date version;//楽観鎖
+
+}
--- team2/src/main/java/com/csc/api/service/impl/test1.java
+++
@@ -1,5 +0,0 @@
-package com.csc.api.service.impl;
-
-public class test1 {
-
-}
