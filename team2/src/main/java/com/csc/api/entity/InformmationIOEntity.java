cd86abc75ec99e567c02a77b324e98932299ebcf
goboei <go.boei@casc.co.jp>
2024-01-17 13:39:07 +0900

    u

---
+++ team2/src/main/java/com/csc/api/entity/InformmationIOEntity.java
@@ -0,0 +1,13 @@
+package com.csc.api.entity;
+
+public class InformmationIOEntity {
+	//m_codeテーブル
+	private String codeKbn;
+	private Integer codeId;
+	private String name;
+	private String delFlg;
+	private String createDate;
+	private String createUesr;
+	private String updateDate;
+	private String updateUser;
+}
